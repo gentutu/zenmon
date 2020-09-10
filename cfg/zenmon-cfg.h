7a9f91bcac857395eb1421e62642169e5e911c98
gentutu <48392013+gentutu@users.noreply.github.com>
2020-09-10 18:48:23 +0300

    Better config defines

--- cfg/zenmon-cfg.h
+++ cfg/zenmon-cfg.h
@@ -14,8 +14,8 @@
 // DEFINES
 //======================================================================================================================
 // graph sizes in dots and characters
-#define LEN_PLOT_D_X    120u                           // graph X size in Braille dots; multiple of 2
-#define LEN_PLOT_D_Y    20u                            // graph Y size in Braille dots; multiple of 4
+#define LEN_PLOT_D_X    120u                          // graph X size in Braille dots; multiple of 2
+#define LEN_PLOT_D_Y     20u                          // graph Y size in Braille dots; multiple of 4
 #define LEN_PLOT_C_X   (LEN_PLOT_D_X / 2u)            // graph X size in characters
 #define LEN_PLOT_C_Y   (LEN_PLOT_D_Y / 4u)            // graph Y size in characters
 #define LEN_PLOT_BUF   (LEN_PLOT_C_X * LEN_PLOT_C_Y)
@@ -28,9 +28,9 @@
 #define LEN_TEMP_W_X  ((LEN_PLOT_D_X / 2u     ) + 4u) // X size in characters
 #define LEN_TEMP_W_Y  ((LEN_PLOT_D_Y / 4u * 4u) + 2u) // Y size in characters
 
-// these are based on the current layout and is used to center the content to the terminal window
-#define LEN_TOTAL_W_X  (LEN_SVI2_W_X + LEN_TEMP_W_X)
-#define LEN_TOTAL_W_Y  (LEN_SVI2_W_Y + LEN_TEMP_W_Y)
+// total display space used; based on the current layout and is used to center the content to the terminal window
+#define LEN_TOTAL_W_X  (LEN_SVI2_W_X + LEN_TEMP_W_X)  // X size in characters
+#define LEN_TOTAL_W_Y  (LEN_SVI2_W_Y + LEN_TEMP_W_Y)  // Y size in characters
 
 //======================================================================================================================
 // END OF FILE
--- lib/logging.h
+++ lib/logging.h
@@ -15,48 +15,46 @@
 //======================================================================================================================
 // DEFINES
 //======================================================================================================================
-// disable this to remove prints and reduce file size; it will make the "-h" argument and all error prints useless
-#define ENABLE_DEBUG
 
-// enable this if you want a lot of console spam with a lot of events
-//#define ENABLE_VERBOSITY
+#define ENABLE_DEBUG     ON  // disable to remove prints and reduce file size; "-h"/error prints will not be shown
+#define ENABLE_VERBOSITY OFF // enable if you want a lot of console spam with a lot of events
 
-#if defined(ENABLE_DEBUG)
+#if(TRUE == ENABLE_DEBUG)
     #define PRINT_OKAY(...) \
         do { printf("[ %sOKAY%s ] ", F_GRN, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
     #define PRINT_OKAY(...)
 #endif
 
-#if defined(ENABLE_DEBUG)
+#if(TRUE == ENABLE_DEBUG)
     #define PRINT_WARN(...) \
         do { printf("[ %sWARN%s ] ", F_YEL, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
     #define PRINT_WARN(...)
 #endif
 
-#if defined(ENABLE_DEBUG)
+#if(TRUE == ENABLE_DEBUG)
     #define PRINT_FAIL(...) \
         do { printf("[ %sFAIL%s ] ", F_RED, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
     #define PRINT_FAIL(...)
 #endif
 
-#if defined(ENABLE_DEBUG)
+#if(TRUE == ENABLE_DEBUG)
     #define PRINT_INFO(...) \
         do { printf("[ %sINFO%s ] ", F_CYN, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
     #define PRINT_INFO(...)
 #endif
 
-#if defined(ENABLE_DEBUG)
+#if(TRUE == ENABLE_DEBUG)
     #define PRINT_TEXT(...) \
         do { printf("  %s    %s   ", F_RST, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
     #define PRINT_TEXT(...)
 #endif
 
-#if defined(ENABLE_DEBUG) && defined(ENABLE_VERBOSITY)
+#if(TRUE == ENABLE_DEBUG) && (ON == ENABLE_VERBOSITY)
     #define PRINT_VERB(...) \
         do { printf("[ %sVERB%s ] ", F_MAG, F_RST); printf(__VA_ARGS__); printf("%s\n", F_RST); } while(0)
 #else
