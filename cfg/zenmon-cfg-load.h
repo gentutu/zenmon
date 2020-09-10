17da7998b208357409cf7715146e94b0c904f1e7
Denis Isai <deniseaisai@gmail.com>
2020-09-10 18:48:23 +0300

    Better config defines

--- cfg/zenmon-cfg-load.h
+++ cfg/zenmon-cfg-load.h
@@ -12,40 +12,42 @@
 // DEFINES
 //======================================================================================================================
 // file paths; these are checked at runtime and if any are missing the program will notify and exit
-#define FILE_MHZ         "/proc/cpuinfo"      // cpu core   frequency
-#define FILE_USG         "/proc/stat"         // cpu thread usage
-#define FILE_RAM         "/proc/meminfo"      // RAM        usage
+#define FILE_MHZ         "/proc/cpuinfo"                 // cpu core   frequency
+#define FILE_USG         "/proc/stat"                    // cpu thread usage
+#define FILE_RAM         "/proc/meminfo"                 // RAM        usage
 
 // load window size
-#define LEN_LOAD_W_X      47u                 // X size in characters
-#define LEN_LOAD_W_Y      25u                 // Y size in characters
+#define LEN_LOAD_W_X        47u                          // X size in characters
+#define LEN_LOAD_W_Y        25u                          // Y size in characters
 
 // static configuration
-#define CPU_CORES         16u
-#define CPU_THREADS       32u
-#define SEG_PER_BOX       8u                  // the full block has 8 segments
-#define LEN_LINE          512u                // I don't expect any lines to be longet than this while reading
+#define CPU_CORES           16u
+#define CPU_THREADS         32u
+#define SEG_PER_BOX          8u                          // the full block has 8 segments
+#define LEN_LINE           512u                          // I don't expect any lines to be longer
+
+#define SUBBARS_PER_BOX      8u                          // number of horizontal/vertical sub/boxes per full box
 
 // RAM usage bar config
-#define RAM_BAR_LEN      (LEN_LOAD_W_X - 4u) // RAM bar length in characters
-#define RAM_BAR_LIM      (RAM_BAR_LEN  / 2u) // bar turns red at 50% RAM usage
-#define RAM_AVAIL_OFFSET  13u                 // characters to skip when reading the available RAM line
-#define RAM_TOTAL_OFFSET  9u                  // characters to skip when reading the total     RAM line
+#define RAM_AVAIL_OFFSET    13u                          // characters to skip when reading the available RAM line
+#define RAM_TOTAL_OFFSET     9u                          // characters to skip when reading the total     RAM line
+#define RAM_BAR_LEN      (LEN_LOAD_W_X - 4u)             // RAM bar length in characters
+#define RAM_BAR_LIM      (RAM_BAR_LEN  / 2u)             // bar turns red at 50% RAM usage
 
 // core MHz bars config
-#define MHZ_CPU_MAX       5000u               // max value of the MHz bar
-#define MHZ_BAR_LEN       5u                  // bar height in lines
-#define MHZ_BAR_LIM       4u                  // bar turns red at 80% MHz of MAX
-#define MHZ_PER_BOX       1000u               // (MHZ_CPU_MAX / MHZ_BAR_LEN)
-#define MHZ_PER_SEG       125u                // (MHZ_PER_BOX / SUBBARS_PER_BOX)
-#define MHZ_OFFSET        10u                 // characters to skip when reading the MHz line
+#define MHZ_OFFSET          10u                          // characters to skip when reading the MHz line
+#define MHZ_CPU_MAX       5000u                          // max value of the MHz bar in MHz
+#define MHZ_BAR_LEN          5u                          // height of the MHz bar in lines
+#define MHZ_BAR_LIM          4u                          // bar turns red at 80% MHz of MAX
+#define MHZ_PER_BOX      (MHZ_CPU_MAX / MHZ_BAR_LEN)     // value of a full bar in MHz
+#define MHZ_PER_SEG      (MHZ_PER_BOX / SUBBARS_PER_BOX) // value of a sub-bar  in MHz
 
 // thread usage bars config; permille instead of percent is used because of high bar granularity
-#define USG_CPU_MAX       1000u               // used for the usg bar
-#define USG_BAR_LEN       5u                  // bar height in lines
-#define USG_BAR_LIM       4u                  // bar turns red at 80% usg of MAX
-#define USG_PER_BOX       200u                // (USG_CPU_MAX / USG_BAR_LEN)
-#define USG_PER_SEG       25u                 // (USG_PER_BOX / SUBBARS_PER_BOX)
+#define USG_CPU_MAX       1000u                          // reference value for 100% load
+#define USG_BAR_LEN          5u                          // height of the usage bar in lines
+#define USG_BAR_LIM          4u                          // bar turns red at 80% usg of MAX
+#define USG_PER_BOX      (USG_CPU_MAX / USG_BAR_LEN)     // value of a full bar in usage
+#define USG_PER_SEG      (USG_PER_BOX / SUBBARS_PER_BOX) // value of a sub-bar  in usage
 
 //======================================================================================================================
 // END OF FILE
--- cfg/zenmon-cfg-svi2.h
+++ cfg/zenmon-cfg-svi2.h
@@ -7,34 +7,34 @@
 // DEFINES
 //======================================================================================================================
 // file paths; these are checked at runtime and if any are missing the program will notify and exit
-#define FILE_VCORE  "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/in0_input"    // core voltage
-#define FILE_ACORE  "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/curr1_input"  // core current
-#define FILE_VSOC   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/in1_input"    // soc  voltage
-#define FILE_ASOC   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/curr2_input"  // soc  current
-#define FILE_CDIE   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp1_input"  // core temperature
-#define FILE_CCTL   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp2_input"  // ctl  temperature
-#define FILE_CCCD0  "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp3_input"  // ccd0 temperature
-#define FILE_CCCD1  "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp4_input"  // ccd1 temperature
+#define FILE_VCORE   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/in0_input"   // core voltage
+#define FILE_ACORE   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/curr1_input" // core current
+#define FILE_VSOC    "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/in1_input"   // soc  voltage
+#define FILE_ASOC    "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/curr2_input" // soc  current
+#define FILE_CDIE    "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp1_input" // core temperature
+#define FILE_CCTL    "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp2_input" // ctl  temperature
+#define FILE_CCCD0   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp3_input" // ccd0 temperature
+#define FILE_CCCD1   "/sys/devices/pci0000:00/0000:00:18.3/hwmon/hwmon0/temp4_input" // ccd1 temperature
 
 // svi2 window size
-#define LEN_SVI2_W_X 47u   // X size in characters
-#define LEN_SVI2_W_Y 14u   // Y size in characters
+#define LEN_SVI2_W_X    47u // X size in characters
+#define LEN_SVI2_W_Y    14u // Y size in characters
 
 // static config
-#define AVG_CYCLES   10u
+#define AVG_CYCLES      10u // measurements until average is displayed
 
 // graph scales and alarm thresholds; these numbers depend of the definitions of graph sizes from zenmon-cfg.h
-#define C_D_VALUE    5u    // each Braille dot represents 5 °C; 0 - 100 scale
-#define LIM_C        60u   // °C; multiple of (C_D_VALUE * LEN_PLOT_D_Y)
+#define C_D_VALUE        5u // each Braille dot represents 5 °C; 0 - 100 scale
+#define LIM_C           60u // °C; multiple of (C_D_VALUE * LEN_PLOT_D_Y); see zenmon-cfg.h
 
-#define V_D_VALUE    75u   // each Braille dot represents 75 millivolts; 0 - 1500 scale
-#define LIM_V        1200u // millivolts; multiple of (V_D_VALUE * LEN_PLOT_D_Y)
+#define V_D_VALUE       75u // each Braille dot represents 75 millivolts; 0 - 1500 scale
+#define LIM_V         1200u // millivolts; multiple of (V_D_VALUE * LEN_PLOT_D_Y); see zenmon-cfg.h
 
-#define A_D_VALUE    8u    // each Braille dot represents 8 amperes; 0 - 160 scale
-#define LIM_A        128   // amperes; multiple of (A_D_VALUE * LEN_PLOT_D_Y)
+#define A_D_VALUE        8u // each Braille dot represents 8 amperes; 0 - 160 scale
+#define LIM_A          128u // amperes; multiple of (A_D_VALUE * LEN_PLOT_D_Y); see zenmon-cfg.h
 
-#define W_D_VALUE    10u   // each Braille dot represents 10 watts; 0 - 200 scale
-#define LIM_W        160u  // watts; multiple of (W_D_VALUE * LEN_PLOT_D_Y)
+#define W_D_VALUE       10u // each Braille dot represents 10 watts; 0 - 200 scale
+#define LIM_W          160u // watts; multiple of (W_D_VALUE * LEN_PLOT_D_Y); see zenmon-cfg.h
 
 //======================================================================================================================
 // END OF FILE
